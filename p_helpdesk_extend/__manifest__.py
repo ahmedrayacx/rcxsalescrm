40fb8d5f805fa6d457926ea72714f8d1193b8aef
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2022-12-30 22:24:02 +0200

    Add files via upload

---
+++ p_helpdesk_extend/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Param Enterprice. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Helpdesk Extend",
+    'version': '15.0.1',
+    'author': "Helpdesk Enterprice",
+    'category': 'Tools',
+    'summary': 'Helpdesk Extend',
+    'description': 'Helpdesk Extend',
+    'depends': [
+        'base',
+        'helpdesk'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'view/helpdesk_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
