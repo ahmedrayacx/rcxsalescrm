79b67088d059c30a4460324b946533661fddec20
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2022-12-31 16:50:02 +0200

    Add files via upload

--- p_helpdesk_extend/models/helpdesk.py
+++ p_helpdesk_extend/models/helpdesk.py
@@ -5,8 +5,8 @@ class Helpdesk_ticket(models.Model):
     _inherit = 'helpdesk.ticket'
     is_IT=fields.Boolean(compute='compute_is_IT')
     CodeName = fields.Char("Code Name")
-    Project = fields.Selection([('new', 'New'), ('existed', 'Existed')], string='Project')
-    Voice = fields.Boolean(string="Voice",default=False)
+    project = fields.Selection([('new', 'New'), ('existed', 'Existed')], string='Project')
+    voice = fields.Boolean(string="Voice",default=False)
     ChatBot = fields.Boolean(string="ChatBot",default=False)
     SmartIVR = fields.Boolean(string="Smart IVR",default=False)
     IVRDeflection  = fields.Boolean(string="IVR Deflection",default=False)
