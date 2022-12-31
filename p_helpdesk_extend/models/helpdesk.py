9a50d7d7d177d9900b47161106d5fdaea9240979
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2022-12-31 12:34:27 +0200

    Add files via upload

--- p_helpdesk_extend/models/helpdesk.py
+++ p_helpdesk_extend/models/helpdesk.py
@@ -7,18 +7,7 @@ class Helpdesk_ticket(models.Model):
     CodeName = fields.Char("Code Name")
    
     Voice = fields.Boolean(string="Voice",default=False)
-    ChatBot = fields.Boolean(string="ChatBot",default=False)
-    SmartIVR = fields.Boolean(string="Smart IVR",default=False)
-    IVRDeflection  = fields.Boolean(string="IVR Deflection",default=False)
-    Transactions = fields.Selection(
-        [('Inbound', 'Inbound'), ('Outbound', 'Outbound'), ('Inbound&Outbound', 'Inbound-Outbound'))
-    NumberofLicenses= fields.Char(
-        "Number of Licenses / Ports"
-    )
-    Attachment = fields.Binary()
-    WhatsApp = fields.Boolean(string="WhatsApp",default=False)
-    Facebook = fields.Boolean(string="Facebook",default=False)
-    WebChat = fields.Boolean(string="WebChat",default=False)
+   
    
    
 
