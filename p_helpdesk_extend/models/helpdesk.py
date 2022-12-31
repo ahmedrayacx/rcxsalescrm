e406faf3117605edfd060c69502603676058366b
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2022-12-31 16:26:44 +0200

    Add files via upload

--- p_helpdesk_extend/models/helpdesk.py
+++ p_helpdesk_extend/models/helpdesk.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 
 class Helpdesk_ticket(models.Model):
     _inherit = 'helpdesk.ticket'
-
+    is_IT=fields.Boolean(compute='compute_is_IT')
     CodeName = fields.Char("Code Name")
     Project = fields.Selection([('new', 'New'), ('existed', 'Existed')], string='Project')
     Voice = fields.Boolean(string="Voice",default=False)
@@ -22,7 +22,14 @@ class Helpdesk_ticket(models.Model):
    
    
    
-
+    @api.depends('team_id')
+    def compute_is_IT(self):
+        for rec in self:
+            
+            if rec.team_id.name=="IT":
+                rec.is_IT=True
+            else:
+                rec.is_IT=False
 
 
 
