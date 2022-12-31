80e03b68c6a261167959420f675edac9651e4ef9
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2022-12-31 11:55:59 +0200

    Add files via upload

--- p_helpdesk_extend/models/helpdesk.py
+++ p_helpdesk_extend/models/helpdesk.py
@@ -5,19 +5,9 @@ class Helpdesk_ticket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     CodeName = fields.Char("Code Name")
+    Project = fields.Selection([('New','New')],('Existed','Existed')])
    
    
-    @api.model
-    def create(self, vals):
-        call_super = super(Helpdesk_ticket, self).create(vals)
-        for rec in call_super:
-            if rec.help_depart_id:
-                email_values = {
-                    'email_to': rec.help_depart_id.email_to,
-                    'email_cc': rec.help_depart_id.email_cc,
-                }
-                rec.help_depart_id.template_id.send_mail(rec.id, force_send=True, email_values=email_values)
-        return call_super
 
 
 
