b2357cf6c609b348b8b9ca05cc67b7b406e27fa1
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2023-01-20 15:39:57 +0200

    Add files via upload

---
+++ p_crm_extend/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Param Enterprice. See LICENSE file for full copyright and licensing details.
+{
+    'name': "CRM Extend",
+    'version': '15.0.1',
+    'author': "Param Enterprice",
+    'category': 'CRM',
+    'summary': 'CRM Extend',
+    'description': 'CRM Extend',
+    'depends': [
+        'crm'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'view/crm_view.xml',
+        'data/data.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
---
+++ p_crm_extend/models/crm.py
@@ -0,0 +1,5 @@
+from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
