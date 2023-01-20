ba129af3a5de5b6c62307eda2b2f0411d74fc78a
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2023-01-20 14:24:34 +0200

    Add files via upload

---
+++ p_contact_extend/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Param Enterprice. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Contact Extend",
+    'version': '15.0.1',
+    'author': "Param Enterprice",
+    'category': 'Tools',
+    'summary': 'Contact Extend',
+    'description': 'Contact Extend',
+    'depends': [
+        'base',
+        'contacts'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'view/contact_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
---
+++ p_contact_extend/models/contact.py
@@ -0,0 +1,56 @@
+import math
+from dateutil.relativedelta import relativedelta
+from random import randint
+from odoo import models, fields
+
+
+class Partner_inherit(models.Model):
+    _inherit = 'res.partner'
+
+    codeName = fields.Char("Code Name")
+    type_of_service_ids = fields.Many2many('contact.typeofservice', string='Type Of Service')
+    market_ids = fields.Many2many('contact.market', string='Market')
+    existingclient = fields.Boolean(string="Existing Client ?",default=False)
+
+
+
+
+
+
+
+
+
+
+
+
+class ContactTypeOfService(models.Model):
+    _name = 'contact.typeofservice'
+    _description = 'contact Type Of Service'
+    _order = 'name'
+
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char(required=True, translate=True)
+    color = fields.Integer('Color', default=_get_default_color)
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Service name already exists !"),
+    ]
+
+
+
+class ContactMarket(models.Model):
+    _name = 'contact.market'
+    _description = 'Lead Market'
+    _order = 'name'
+
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char(required=True, translate=True)
+    color = fields.Integer('Color', default=_get_default_color)
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Market name already exists !"),
+    ]
