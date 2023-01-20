58a789ef550fe4171f826cc14acf51b6ff4b684a
ahmedrayacx <99891789+ahmedrayacx@users.noreply.github.com>
2023-01-20 14:38:32 +0200

    Add files via upload

--- p_helpdesk_extend/models/helpdesk.py
+++ p_helpdesk_extend/models/helpdesk.py
@@ -7,8 +7,8 @@ from odoo import models, fields, api
 class Helpdesk_ticket(models.Model):
     _inherit = 'helpdesk.ticket'
     is_IT=fields.Boolean(compute='compute_is_IT')
-    codeName = fields.Char("Code Name")
-    #project = fields.Selection([('new', 'New'), ('existed', 'Existed')], string='Project')
+
+ 
     ticketType = fields.Selection([('inquery', 'Inquery'), ('requestsolution', 'Request Solution')], string='Ticket Type')
     voice = fields.Boolean(string="Voice",default=False)
     chatBot = fields.Boolean(string="ChatBot",default=False)
@@ -38,10 +38,7 @@ class Helpdesk_ticket(models.Model):
     pendingstage = fields.Selection([('proposal', 'Proposal'), ('integration', 'Integration'),('sow', 'SOW'),('solutiondesign', 'Solution Design'),('demo', 'Demo'),('poc', 'POC'),('finalized', 'Finalized')], string='Pending Stage')
     
     
-    
-   # market=fields.Selection([('egypt', 'Egypt'), ('gcc', 'GCC'),('uae','UAE'),('morroco','Morroco'),('ksa', 'KSA'),
-   # ('bahrain','Bahrain'),('kuwait','Kuwait'),('australia','Australia'),('mea','MEA'),('usa','USA'),
-    #('europeuk','Europe/UK'),('oman','Oman'),('qatar','Qatar')], string='Market')
+ 
     industry= fields.Selection(
         [('travel', 'Travel'), ('printingIT', 'Printing / IT'), ('hotelsresort', 'Hotels/Resort'),('fintech','Fintech'),
         ('medical','Medical'),('education','Education'),('hospitality','Hospitality'),('logistics','Logistics'),
@@ -52,15 +49,11 @@ class Helpdesk_ticket(models.Model):
         ('airline','Airline'),('realestate','Real estate'),('mediaentertainment','Media & Entertainment'),
         ('insurtech','Insurtech'),('technology','Technology'),('shipping','Shipping & Logistics'),('manufacturing','Manufacturing'),
         ('printing','Printing'),('fashion','Fashion'),('consumerelectronics','Consumer Electronics')],string='Industry')
-    #type_of_service=fields.Selection([('bPO', 'BPO'), ('outboundcampaign', 'Outbound Campaign'),('hro','HRO'),('ito','ITO'),('hosting', 'Hosting')], string='Type of Service')
     
-    type_of_service_ids = fields.Many2many('helpdesk.typeofservice', string='Type Of Service')
-    market_ids = fields.Many2many('helpdesk.market', string='Market')
     solution=fields.Selection([('newsolution', 'New Solution'), ('updatedsolution', 'Updated Solution')], string='Solution')
     presalesstatus=fields.Selection([('new', 'New'), ('inprogress', 'In Progress'),('closed', 'Closed')], string='Presales Status')
     rfp = fields.Boolean("RFP",default=False)
     rfpsubmissiondate=fields.Date(string="RFP Submission Date")
-    existingclient = fields.Boolean(string="Existing Client ?",default=False)
     rfpontime = fields.Boolean(string="RFP (On-time)?",default=False)
     date_received=fields.Date(string="Date Received")
     date_closed=fields.Date(string="Date Closed")
@@ -80,37 +73,6 @@ class Helpdesk_ticket(models.Model):
 
 
 
-class HelpdeskTypeOfService(models.Model):
-    _name = 'helpdesk.typeofservice'
-    _description = 'Helpdesk Type Of Service'
-    _order = 'name'
-
-    def _get_default_color(self):
-        return randint(1, 11)
-
-    name = fields.Char(required=True, translate=True)
-    color = fields.Integer('Color', default=_get_default_color)
-
-    _sql_constraints = [
-        ('name_uniq', 'unique (name)', "Service name already exists !"),
-    ]
-
-
-
-class HelpdeskMarket(models.Model):
-    _name = 'helpdesk.market'
-    _description = 'Lead Market'
-    _order = 'name'
-
-    def _get_default_color(self):
-        return randint(1, 11)
-
-    name = fields.Char(required=True, translate=True)
-    color = fields.Integer('Color', default=_get_default_color)
-
-    _sql_constraints = [
-        ('name_uniq', 'unique (name)', "Market name already exists !"),
-    ]
 
 
 
